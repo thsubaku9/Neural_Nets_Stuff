6085be219352b6dfdc55940fda7f011637175443
Kaustubh J <kaustubhkj@gmail.com>
2020-05-04 17:25:50 +0530

    SOM

---
+++ Credit Fraud using SOM/mySom.py
@@ -0,0 +1,43 @@
+#mysom
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Credit_Card_Applications.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
+
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+X = sc.fit_transform(X)
+
+from minisom import MiniSom
+
+som = MiniSom(x = 20, y = 20, input_len = 15, sigma = 1.0, learning_rate = 0.5)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100)
+
+from pylab import bone, pcolor, colorbar, plot, show
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o', 's']
+colors = ['r', 'g']
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0] + 0.5,
+         w[1] + 0.5,
+         markers[Y[i]],
+         markeredgecolor = colors[Y[i]],
+         markerfacecolor = 'None',
+         markersize = 10,
+         markeredgewidth = 2)
+show()
+
+
+mappings = som.win_map(X)
+#12,10 and 9,18 are chosen on the basis of high IND values, will differ based on MiniSom size
+frauds = np.concatenate((mappings[(12,10)], mappings[(9,18)]), axis = 0)
+frauds = sc.inverse_transform(frauds)
+
