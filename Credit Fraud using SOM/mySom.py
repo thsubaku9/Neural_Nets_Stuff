989e28b35d3408e21ddfb9f556bd33d1bb87502f
Kaustubh J <kaustubhkj@gmail.com>
2020-05-15 16:32:56 +0530

    remember to add save weights for later use

--- Credit Fraud using SOM/mySom.py
+++ Credit Fraud using SOM/mySom.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 dataset = pd.read_csv('Credit_Card_Applications.csv')
 X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, -1].values
@@ -35,9 +36,59 @@ for i, x in enumerate(X):
          markeredgewidth = 2)
 show()
 
-
 mappings = som.win_map(X)
+
+threshold = 0.5
+
+myData = list()
+
+distance = som.distance_map().T
+
+for i in range(distance.shape[0]):
+    avg_dist = distance[i].sum()/distance[i].shape
+    if (avg_dist >= threshold):
+        j = distance[i].argmax()
+
+        if(len(mappings[(i,j)]) > 0):
+            for x in mappings[(i,j)]:
+                myData.append(x)
+
+myData = np.array(myData)
 #12,10 and 9,18 are chosen on the basis of high IND values, will differ based on MiniSom size
-frauds = np.concatenate((mappings[(12,10)], mappings[(9,18)]), axis = 0)
+frauds = np.concatenate((myData),axis = 0)
+frauds = frauds.reshape(myData.shape)
 frauds = sc.inverse_transform(frauds)
 
+
+#Unsuper -> super
+customers = dataset.iloc[:, 1:].values
+is_fraud = np.zeros(len(dataset))
+
+for i in range(len(dataset)):
+    if np.intersect1d(dataset.iloc[i,0],frauds).size >0 :
+        is_fraud[i] = 1
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+customers = sc.fit_transform(customers)
+
+from keras.layers import Dense,Dropout
+from keras.models import Input,Model
+from keras import losses,optimizers
+
+
+feedIn = Input(shape = (customers.shape[1],))
+
+layer_1 = Dense(units = 3, kernel_initializer = 'uniform', activation = 'relu')(feedIn)
+output_layer = Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid')(layer_1)
+model = Model(feedIn,output_layer)
+model.compile(optimizer = optimizers.Adam(), loss = losses.binary_crossentropy, metrics = ['accuracy'])
+model.fit(customers, is_fraud, batch_size = 2, epochs = 3)
+
+y_pred = model.predict(customers)
+c_loss = y_pred - is_fraud.reshape((690,1))
+a_error = c_loss.sum() / c_loss.shape[0]
+print("Average prediciton error:{} ".format(a_error))
+
+y_pred = np.concatenate((dataset.iloc[:, 0:1].values, y_pred), axis = 1)
+y_pred = y_pred[y_pred[:, 1].argsort()]
